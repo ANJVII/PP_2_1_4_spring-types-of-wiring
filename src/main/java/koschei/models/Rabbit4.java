425641f07c82248192791ac30569173d0c858502
ANJVII <118735231+ANJVII@users.noreply.github.com>
2023-01-29 20:41:49 +0800

    Add files via upload

---
+++ src/main/java/koschei/models/Rabbit4.java
@@ -0,0 +1,19 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Rabbit4 {
+
+    private Duck5 duk;
+
+    @Autowired
+    public void setDuk(Duck5 duk) {
+        this.duk = duk;
+    }
+    @Override
+    public String toString() {
+        return ", в зайце утка " + "";
+    }
+}
